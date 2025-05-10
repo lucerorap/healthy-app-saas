18e5f92d03aedff997f1d9c79a6003d4447b38c2
Adrian Rafael Lucero <adrianrafaellucero9@gmail.com>
2025-05-10 19:42:56 +0800

    backend: Admin Dashboard

--- backend/resources/views/admin/dashboard.blade.php
+++ backend/resources/views/admin/dashboard.blade.php
@@ -1,1 +1,160 @@
-dashboard
+@extends('admin.layouts.app')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+<div class="row my-5">
+    <div class="col-md-4">
+        <!-- sidebar here -->
+        @include('admin.layouts.sidebar')
+    </div>
+    <div class="col-md-8">
+        <div class="container">
+            <div class="row ">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-blue-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-brands fa-product-hunt"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Products</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- products count here -->
+                                        {{ $products->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-orange-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-regular fa-face-smile-beam"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Positives</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- positives count here -->
+                                        {{ $positives->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-green-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-regular fa-face-frown"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Negatives</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- negatives count here -->
+                                        {{ $negatives->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-yellow-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-clock-rotate-left"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Histories</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- histories count here -->
+                                        {{ $histories->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-red-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-users"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Users</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- users count here -->
+                                        {{ $users->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-moonlight-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-chart-gantt"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Plans</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- plans count here -->
+                                        {{ $plans->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="card dashboard_card l-bg-pink-dark">
+                        <div class="card-statistic-3 p-4">
+                            <div class="card-icon card-icon-large">
+                                <i class="fa-solid fa-dollar-sign"></i>
+                            </div>
+                            <div class="mb-4">
+                                <h5 class="card-title mb-0">Subscriptions</h5>
+                            </div>
+                            <div class="row align-items-center mb-2 d-flex">
+                                <div class="col-8">
+                                    <h2 class="d-flex align-items-center mb-0">
+                                        <!-- subscriptions count here -->
+                                        {{ $subscriptions->count() }}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ backend/resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,62 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark rounded" style="width: 280px;">
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li>
+            <a href="{{ route('admin.index') }}" class="nav-link text-white">
+                <i class="fas fa-dashboard"></i> Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa-brands fa-product-hunt"></i> Products
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa-regular fa-face-smile-beam"></i> Positives
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa-regular fa-face-frown"></i> Negatives
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa-solid fa-users"></i> Users
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa-solid fa-clock-rotate-left"></i> Histories
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa-solid fa-chart-gantt"></i> Plans
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link text-white">
+                <i class="fa fa-dollar-sign"></i> Subscriptions
+            </a>
+        </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://cdn.pixabay.com/photo/2021/07/02/04/48/user-6380868_1280.png" alt="" width="32" height="32" class="rounded-circle me-2">
+            <strong>
+                <!-- admin name here -->
+                {{ auth()->guard('admin')->user()->name }}
+            </strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li>
+                <form id="adminLogout" action="#" method="post">
+                </form>
+                <a class="dropdown-item" href="#"
+                >Sign out</a>
+            </li>
+        </ul>
+    </div>
+</div>
